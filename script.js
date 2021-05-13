84ac7f191a512fce63a457563671019d85aa5703
WalkerL2000 <walker.lambert@cca.edu>
2021-05-12 19:37:38 -0700

    fianlly

---
+++ script.js
@@ -0,0 +1,136 @@
+console.log('artmode');
+
+$(document).ready(function(){
+
+	$(".stripe1").on('mouseover', function() {
+    $(".stripe1").removeClass('stripe1');
+    $(this).toggleClass('active');
+
+	});
+
+	$(".stripe2").on('mouseover', function() {
+    $(".stripe2").removeClass('stripe2');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe3").on('mouseover', function() {
+    $(".stripe3").removeClass('stripe3');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe4").on('mouseover', function() {
+    $(".stripe4").removeClass('stripe4');
+    $(this).toggleClass('active');
+
+	});
+
+    $(".stripe5").on('mouseover', function() {
+    $(".stripe5").removeClass('stripe5');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe6").on('mouseover', function() {
+    $(".stripe6").removeClass('stripe6');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe7").on('mouseover', function() {
+    $(".stripe7").removeClass('stripe7');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe8").on('mouseover', function() {
+    $(".stripe8").removeClass('stripe8');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe9").on('mouseover', function() {
+    $(".stripe9").removeClass('stripe9');
+    $(this).toggleClass('active');
+
+    });
+
+    $(".stripe10").on('mouseover', function() {
+    $(".stripe10").removeClass('stripe10');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe11").on('mouseover', function() {
+    $(".stripe11").removeClass('stripe11');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe12").on('mouseover', function() {
+    $(".stripe12").removeClass('stripe12');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe13").on('mouseover', function() {
+    $(".stripe13").removeClass('stripe13');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe14").on('mouseover', function() {
+    $(".stripe14").removeClass('stripe14');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe15").on('mouseover', function() {
+    $(".stripe15").removeClass('stripe15');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe16").on('mouseover', function() {
+    $(".stripe16").removeClass('stripe16');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe17").on('mouseover', function() {
+    $(".stripe17").removeClass('stripe17');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe18").on('mouseover', function() {
+    $(".stripe18").removeClass('stripe18');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe19").on('mouseover', function() {
+    $(".stripe19").removeClass('stripe19');
+    $(this).toggleClass('active');
+
+	});
+
+	 $(".stripe20").on('mouseover', function() {
+    $(".stripe20").removeClass('stripe20');
+    $(this).toggleClass('active');
+
+	});
+
+	$("#darkmode").click(function(){
+		$('body').toggleClass('darkmode');
+	});
+
+
+	$("#darkmode").click(function(){
+		$('.stripes').toggleClass('darkmode');
+	});
+	 
+
+
+});
